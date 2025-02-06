b4b55685b12f8ad50e21ff0e0ef1f6621ca13a15
Mahin Bisht <mahinbisht1997@gmail.com>
2025-02-06 19:57:57 +0530

    internal error 5000

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,23 @@
+﻿# Use an official Python runtime as a parent image
 FROM python:3.10-slim
 
+# Set the working directory inside the container
 WORKDIR /app
 
+# Copy the requirements file to the container
 COPY requirements.txt .
 
-RUN pip install -r requirements.txt --no-cache-dir
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
 
+# Copy the rest of the application code to the container
 COPY . .
 
+#ARG MONGO_URI
+#ENV MONGO_URI=${MONGO_URI}
+
+# Expose the port FastAPI runs on
 EXPOSE 8000
 
+# Command to run the application
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- database/db.py
+++ database/db.py
@@ -1,6 +1,23 @@
-import os
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-MONGO_URL = os.getenv("MONGODB_URI", "mongodb://mongodb:27017/mydatabase")  # Use Docker service name
-client = MongoClient(MONGO_URL)
-db = client["attendance_db"]
+# Load environment variables from .env file
+load_dotenv()
+
+class Database:
+    client: MongoClient = None
+    db_name: str = "testdb"
+
+    def connect(self):
+        mongo_uri = os.environ.get("MONGO_URI")
+        if not mongo_uri:
+            raise ValueError("MONGO_URI is not set in the environment variables.")
+        self.client = MongoClient(mongo_uri)
+
+    def get_client(self) -> MongoClient:
+        if not self.client:
+            self.connect()
+        return self.client
+
+db = Database()
