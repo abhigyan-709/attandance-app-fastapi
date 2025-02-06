ca76e5264f35006773fa56f173372b240b8cd580
Mahin Bisht <mahinbisht1997@gmail.com>
2025-02-06 19:57:57 +0530

    internal error 5000

--- models/attendance.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class Attendance(BaseModel):
-    username: str
-    timestamp: datetime
--- models/user.py
+++ models/user.py
@@ -1,27 +1,16 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional
+
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
     first_name: str
     last_name: str
-    email: EmailStr
-    phone: str
-    district: str
+    city: str
     username: str
+    email: EmailStr
     password: str
-    activated: bool = False
-    user_type: str  # "admin" or "user"
+    role : str = "user" # default user is set to the user role
+    is_active : bool = False
 
     class Config:
-        schema_extra = {
-            "example": {
-                "first_name": "John",
-                "last_name": "Doe",
-                "email": "john@example.com",
-                "phone": "+1234567890",
-                "district": "New York",
-                "username": "john_doe",
-                "activated": True,
-                "user_type": "user"
-            }
-        }
+        orm_mode = True
