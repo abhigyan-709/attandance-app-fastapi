f004dd3382c4eec2b384d45105e757a7654f69e9
abhigyan-709 <abhigkumar709@gmail.com>
2025-02-09 19:18:59 +0530

    db updated

--- routes/visitors.py
+++ routes/visitors.py
@@ -15,13 +15,8 @@ async def get_visitor_count():
 @router4.post("/increment-visitor")
 async def increment_visitor_count():
     """Increment the visitor count and return the updated value."""
+    db_client = db.get_client()
+    db_client[db.db_name]["visitors"].update_one({"_id": "visitor_count"}, {"$inc": {"count": 1}})
     visitors = db.visitors.find_one({"_id": "visitor_count"})
-    
-    if not visitors:
-        db.visitors.insert_one({"_id": "visitor_count", "count": 1})
-        return {"count": 1}
+    return {"count": visitors["count"]}
 
-    new_count = visitors["count"] + 1
-    db.visitors.update_one({"_id": "visitor_count"}, {"$set": {"count": new_count}})
-    
-    return {"count": new_count}
