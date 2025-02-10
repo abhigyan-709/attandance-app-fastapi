eea2917242b50f27bf681ed41d3195cdb97e05f0
abhigyan-709 <abhigkumar709@gmail.com>
2025-02-11 01:15:31 +0530

    get message for admin

--- routes/message.py
+++ routes/message.py
@@ -9,6 +9,7 @@ from pymongo import MongoClient
 import secrets
 from bson import ObjectId
 from fastapi.responses import JSONResponse
+from routes.user import get_current_user 
 
 route3 = APIRouter()
 
@@ -18,3 +19,20 @@ async def create_message(message: Message):
     db_client[db.db_name]["messages"].insert_one(message.dict())
     return JSONResponse(content={"message": "Message sent successfully"}, status_code=201)
 
+@route3.get("/messages", tags=["Message"])
+async def get_messages(current_user: User = Depends(get_current_user)):
+
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=403,
+            detail="You do not have permission to access this resource"
+        )
+    
+    db_client = db.get_client()
+    messages = db_client[db.db_name]["messages"].find()
+    messages_list = []
+    for message in messages:
+        message["_id"] = str(message["_id"])
+        messages_list.append(message)
+
+    return messages_list
