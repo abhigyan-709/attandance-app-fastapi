a91b91f0abc68595e89cb78a16c7372e8e104710
abhigyan-709 <abhigkumar709@gmail.com>
2025-02-21 19:56:25 +0530

    added new staging changes

--- routes/subscription.py
+++ routes/subscription.py
@@ -5,6 +5,8 @@ from fastapi.responses import JSONResponse
 from pymongo import MongoClient
 from routes.send_email import send_registration_email ,send_password_reset_email
 from passlib.context import CryptContext
+from routes.user import get_current_user
+from models.user import User
 
 route21 = APIRouter()
 
@@ -19,38 +21,28 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 @route21.post("/course-subscription/", tags = ["Paid User Subscription"])
-async def course(course : Courses):
+async def course(course : Courses, current_user = Depends(get_current_user)):
+    # check is the user is admin or not
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=403,
+            detail="You do not have permission to add a Google Meet Link"
+        )
+    
     db_client = db.get_client()
     db_client[db.db_name]["courses"].insert_one(course.dict())
     return JSONResponse(content={"message": "Course Inserted Successfully"}, status_code=201)
 
+@route21.get("/get_course_details", tags = ["Paid User Subscription"])
+async def get_course_details():
+    db_client = db.get_client()
+    courses = db_client[db.db_name]["courses"].find()
+    courses_list = []
+    for course in courses:
+        course["_id"] = str(course["_id"])
+        courses_list.append(course)
 
-# @route21.post("/subscription/", tags = ["Paid User Subscription"])
-# async def subscription(user: UserSubscription, db_client: MongoClient = Depends(db.get_client)):
-#     # Check if the username is already taken
-#     existing_user_username = db_client[db.db_name]["user_subscription"].find_one({"username": user.username})
-#     if existing_user_username:
-#         raise HTTPException(status_code=400, detail="Username is already taken")
-
-#     # Check if the email is already taken
-#     existing_user_email = db_client[db.db_name]["user_subscription"].find_one({"email": user.email})
-#     if existing_user_email:
-#         raise HTTPException(status_code=400, detail="Email is already registered")
-
-#     # Hash the password before storing it in the database
-#     user_dict = user.dict()
-#     user_dict['password'] = get_password_hash(user.password)
-    
-#     # Insert the user into the database
-#     result = db_client[db.db_name]["user_subscription"].insert_one(user_dict)
-    
-#     # Convert ObjectId to string
-#     user_dict["_id"] = str(result.inserted_id)
-
-#     await send_registration_email(user.email, user.first_name, user.last_name)
-
-#     # Return response with the user data
-#     return JSONResponse(content=user_dict, status_code=201)
+    return courses_list
 
 @route21.post("/subscription/", tags=["Paid User Subscription"])
 async def subscription(user: UserSubscription, db_client: MongoClient = Depends(db.get_client)):
