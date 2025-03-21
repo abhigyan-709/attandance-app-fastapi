6a07111033973e8912b89ce280fae60d4f409a7a
abhigyan-709 <abhigkumar709@gmail.com>
2025-03-21 18:03:09 +0530

    linkes count implemented

--- models/blogs.py
+++ models/blogs.py
@@ -51,6 +51,8 @@ class BlogPost(BaseModel):
     comments: Optional[List[Comment]] = []  # Fetch comments while getting blogs
     views: Optional[int] = 0  # New field for total view count
     viewed_ips: Optional[List[str]] = []  # New field to track IPs
+    likes: int = 0           # Added
+    liked_ips: List[str] = [] # Added
 
 class Category(BaseModel):
     id: Optional[str] = Field(default=None, alias="_id")
