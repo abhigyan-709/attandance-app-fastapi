cc208b1366702fb71c88bfe2a561a94620506313
abhigyan-709 <abhigkumar709@gmail.com>
2025-07-23 21:29:01 +0530

    ACL Updated for Upload

--- routes/testimonial.py
+++ routes/testimonial.py
@@ -44,7 +44,7 @@ async def create_testimonial(
             file.file,
             AWS_BUCKET_NAME,
             unique_filename,
-            ExtraArgs={"ContentType": file.content_type, "ACL": "public-read"}
+            ExtraArgs={"ContentType": file.content_type}
         )
         image_url = f"https://{AWS_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{unique_filename}"
     except Exception as e:
