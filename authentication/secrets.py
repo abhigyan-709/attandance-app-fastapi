f06ec7a59617e2d53dc7aed5e9d524cb52d1e7bd
abhigyan-709 <abhigkumar709@gmail.com>
2025-08-16 16:17:38 +0530

    minor big fixes

--- authentication/secrets.py
+++ authentication/secrets.py
@@ -18,6 +18,7 @@ import boto3
 import json
 from botocore.exceptions import ClientError
 
+
 def get_secret_value(secret_name: str, region="ap-south-1") -> dict:
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=region)
@@ -29,14 +30,17 @@ def get_secret_value(secret_name: str, region="ap-south-1") -> dict:
         print(f"Error fetching secret {secret_name}: {e}")
         return {}
 
+
 # ✅ FOR GOOGLE OAUTH TOKEN VERIFICATION
 def get_google_oauth_secrets() -> dict:
     return get_secret_value("google_oauth_secret")
+
 
 # ✅ FOR JWT TOKENS
 def get_google_access_secret() -> str:
     data = get_secret_value("google_access_secret")
     return data.get("ACCESS_SECRET_KEY", "fallback-access-secret")
+
 
 def get_google_refresh_secret() -> str:
     data = get_secret_value("google_refresh_secret")
