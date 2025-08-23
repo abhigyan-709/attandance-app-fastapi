307aa1be267536928221d0fcee2d4281afa30795
abhigyan-709 <abhigkumar709@gmail.com>
2025-08-23 19:19:19 +0530

    rollback

--- routes/user.py
+++ routes/user.py
@@ -70,7 +70,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
         user = User(**user_from_db)  # Convert database response to User model
 
-        user.id = str(user_from_db.get("_id", ""))  # fall back
+        # user.id = str(user_from_db.get("_id", ""))  # fall back
         return user
     except jwt.ExpiredSignatureError:
         raise credentials_exception
