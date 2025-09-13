6b9e8acb2768b5698f4a8cec6b32718cab451610
abhigyan-709 <abhigkumar709@gmail.com>
2025-09-13 15:12:09 +0530

    demo

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,21 +1,4 @@
-# # authentication/auth.py
-# from passlib.context import CryptContext
-# from datetime import datetime, timedelta
-# from jose import jwt
-
-# SECRET_KEY = "your_secret_key"
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 120
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# def get_password_hash(password):
-#     return pwd_context.hash(password)
-
-# def verify_password(plain_password, hashed_password):
-#     return pwd_context.verify(plain_password, hashed_password)
-
-
-
+# authentication/auth.py
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
