34cce7f57b437e6adb3bc7776c8a05fa2b32969f
abhigyan-709 <abhigkumar709@gmail.com>
2025-09-13 15:06:33 +0530

    test deployment

--- authentication/auth.py
+++ authentication/auth.py
@@ -14,11 +14,6 @@
 # def verify_password(plain_password, hashed_password):
 #     return pwd_context.verify(plain_password, hashed_password)
 
-# def create_access_token(data: dict, expires_delta: timedelta = None):
-#     to_encode = data.copy()
-#     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=30))
-#     to_encode.update({"exp": expire})
-#     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 from passlib.context import CryptContext
