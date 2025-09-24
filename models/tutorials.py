d6e9eec8268874b373ab163350911fd76c3899c5
abhigyan-709 <abhigkumar709@gmail.com>
2025-09-24 17:45:05 +0530

    bug fixes

--- models/tutorials.py
+++ models/tutorials.py
@@ -79,16 +79,22 @@ class TutorialComment(BaseModel):
 
 
 # ---------- Categories ----------
+# class TutorialCategory(BaseModel):
+#     id: Optional[str] = Field(default=None, alias="_id")
+#     name: str
+#     description: Optional[str] = None
+#     icon: Optional[str] = None
+
+#     class Config:
+#         arbitrary_types_allowed = True
+#         json_encoders = { "ObjectId": str }
+#         alias_generator = lambda x: "_id" if x == "id" else x
+
 class TutorialCategory(BaseModel):
     id: Optional[str] = Field(default=None, alias="_id")
     name: str
     description: Optional[str] = None
     icon: Optional[str] = None
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = { "ObjectId": str }
-        alias_generator = lambda x: "_id" if x == "id" else x
 
 
 # ---------- Progress / Bookmarks (per-user) ----------
